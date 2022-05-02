e2413a8cc483a6ea9a69f673ba6bf803fbabface
Robinho1997 <robin.kempen@gmx.de>
2022-05-02 02:27:27 +0200

    Funktion um die mathematische Eingabe zu lösen, diese im ergebnissFeld anzeigen zu lassen

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function showCalculation(number) {
     let ergebnissFeld = document.getElementById("ergebnissfeld");
     ergebnissFeld.innerHTML += number;
 }
+
+
+function ausrechnen() {
+    let ergebnissFeld = document.getElementById("ergebnissfeld");
+    let ergebniss = eval(ergebnissFeld.innerHTML);
+    ergebnissFeld.innerHTML = ergebniss;
+}
